5d2b70529a97965dde3f50173c9cf7c4d33def47
IrvingDelgadillo <1725110181@utectulancingo.edu.mx>
2026-05-13 14:48:38 +0000

    CREATED ejercicio hola_mundo.py

---
+++ ejercicios/semana_02/hola_mundo.py
@@ -0,0 +1,9 @@
+class NombreClase:
+    def __init__(self):
+        print("Constructor")
+
+def metodoUno(self):
+    print("Metodo Uno")
+
+nombre_objeto = NombreClase()
+nombre_objeto.metodoUno()
